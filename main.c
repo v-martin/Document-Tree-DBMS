5f1064d5f58dc69cfbe531805146d8bac80ca47d
AboHolo <336772@niuitmo.ru>
2023-10-04 02:35:25 +0300

    feat: alloc and free db

--- main.c
+++ main.c
@@ -1,31 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include "main.h"
 
 #define PAGE_SIZE 4096
 
-void create_db(const char *filename) {
+
+struct allocator_t* allocator_init(const char* filename) {
     FILE *file = fopen(filename, "wb");
     if (file == NULL) {
         perror("Error opening file");
-        return;
+        return NULL;
     }
-
-    char page[PAGE_SIZE];
-    memset(page, 0, PAGE_SIZE);
-
-    size_t bytes_written = fwrite(page, 1, PAGE_SIZE, file);
-    if (bytes_written != PAGE_SIZE) {
-        perror("Error writing to file");
+    struct allocator_t* allocator = malloc(sizeof(struct allocator_t));
+    if (allocator == NULL) {
+        perror("");
+        return NULL;
     }
+    allocator->file = file;
+    return allocator;
+}
 
-    fclose(file);
+operation_status allocator_free(struct allocator_t* allocator) {
+    assert(allocator != NULL);
+    FILE* file = allocator->file;
+    if (file == NULL) {
+        return ERROR;
+    }
+    int status = fclose(file);
+    if (status == 0) {
+        free(allocator);
+        return SUCCESS;
+    }
+    return ERROR;
+}
+
+struct database_t* db_init(const char* filename) {
+    assert(filename != NULL);
+    struct database_t* database = malloc(sizeof(struct database_t));
+    if (database == NULL) {
+        return NULL;
+    }
+    database->allocator = allocator_init(filename);
+    if (database->allocator == NULL){
+        free(database->allocator);
+        free(database);
+        return NULL;
+    }
+    return database;
+}
+
+operation_status db_free(struct database_t* database) {
+    assert(database != NULL);
+    operation_status status = allocator_free(database->allocator);
+    if (status == ERROR){
+        free(database);
+        return ERROR;
+    }
+    free(database);
+    return SUCCESS;
+}
+
+struct record write_record(const struct database_t database) {
+
 }
 
 int main() {
     const char *filename = "data.txt";
-    create_db(filename);
-    printf("Page of size %d bytes written to %s\n", PAGE_SIZE, filename);
+    struct database_t* database = db_init(filename);
+    operation_status deletion_status = db_free(database);
+    if (deletion_status == ERROR) {
+        perror("Big L");
+    }
+    printf("Big W");
     return 0;
 }
--- main.h
+++ main.h
@@ -9,26 +9,55 @@
 
 #endif //DOCUMENTTREE_MAIN_H
 
-enum data_type {
-    bool_t = 0,
+typedef enum {
+    bool_t,
     int_t,
     string_t,
     struct_t,
     float_t,
+} data_type;
+
+typedef enum {
+    FILE_STATUS_OK = 0,
+    FILE_STATUS_ALREADY_EXISTS = 1,
+    FILE_STATUS_NOT_EXIST = 2,
+    FILE_STATUS_WRONG_FORMAT = 3,
+    FILE_STATUS_UNABLE_OPEN = 4,
+    FILE_STATUS_UNABLE_RELEASE = 5,
+    FILE_STATUS_ERROR = 6,
+} file_status;
+
+typedef enum {
+    SUCCESS,
+    ERROR,
+} operation_status;
+
+struct allocator_t {
+    FILE *file;
+
 };
 
-struct metadata {
-
-};
-
-struct db_header {
+struct db_header_t {
     uint64_t key;
     enum data_type* types;
 };
 
-struct database {
+struct database_t {
     char* name;
-    struct db_header* header;
+    struct db_header_t* header;
+    struct allocator_t* allocator;
 };
 
+struct record {
 
+};
+
+struct page_header {
+
+};
+
+struct page {
+    struct page_header header;
+    struct record* first_record;
+    struct record* last_record;
+};

9723c8bd518ee01a94bb6af20f179b19a30c3a53
AboHolo <336772@niuitmo.ru>
2023-10-13 16:46:10 +0300

    Fix: restires buffer content

--- buffer/buffer.c
+++ buffer/buffer.c
@@ -2,5 +2,59 @@
 // Created by AboHolo on 13-Oct-23.
 //
 
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include "buffer.h"
 
+buffer_t *buffer_init(uint64_t size) {
+    buffer_t  *buffer = malloc(sizeof(buffer_t));
+    if (buffer == NULL) {
+        return NULL;
+    }
+    buffer->size = size;
+    if (size > 0) {
+        buffer->data = malloc(size);
+        if (buffer->data == NULL) {
+            free(buffer);
+            return NULL;
+        }
+    }
+    else {
+        buffer->data = NULL;
+    }
+    return buffer;
+}
+
+void buffer_free(buffer_t *buffer) {
+    assert(buffer != NULL);
+    free(buffer->data);
+    buffer->data = NULL;
+    buffer->size = 0;
+    free(buffer);
+}
+
+op_status buffer_write(buffer_t *buffer, const void *data, uint64_t data_size) {
+    assert(buffer != NULL);
+    assert(data != NULL);
+    uint64_t new_size = buffer->size + data_size;
+    char *new_data = (char *)realloc(buffer->data, new_size);
+    if (new_data == NULL) {
+        return ERROR;
+    }
+    buffer->data = new_data;
+    buffer->size = new_size;
+    memcpy(buffer->data + buffer->size - data_size, data, data_size);
+    return SUCCESS;
+}
+
+void *buffer_read(const buffer_t *buffer, uint64_t offset) {
+    assert(buffer != NULL);
+    if (offset >= buffer->size) {
+        return NULL;
+    }
+    char *string = buffer->data + offset;
+
+    return string;
+}
 
--- buffer/buffer.h
+++ buffer/buffer.h
@@ -5,4 +5,24 @@
 #ifndef DOCUMENTTREE_BUFFER_H
 #define DOCUMENTTREE_BUFFER_H
 
+#include <stdint.h>
+
+typedef struct {
+    uint64_t size;
+    char *data;
+}buffer_t;
+
+typedef enum {
+    SUCCESS,
+    ERROR,
+} op_status;
+
+buffer_t *buffer_init(uint64_t size);
+
+void buffer_free(buffer_t *buffer);
+
+op_status buffer_write(buffer_t *buffer, const void *data, uint64_t data_size);
+
+void *buffer_read(const buffer_t *buffer, uint64_t offset);
+
 #endif //DOCUMENTTREE_BUFFER_H
